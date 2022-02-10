a0e31fbe3848ef75f3ee1d9dfed26c95e8787b60
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-10 10:19:56 -0800

    add foreign keys to associations

--- models/index.js
+++ models/index.js
@@ -3,27 +3,27 @@ const Comment = require('./Comment');
 const Post = require('./Post');
 
 User.hasMany(Post, {
-  foreignKey: 'gallery_id',
+  foreignKey: 'user_id',
 });
 
 Post.belongsTo(User, {
-  foreignKey: 'gallery_id',
+  foreignKey: 'user_id',
 });
 
 User.hasMany(Comment, {
-  foreignKey: 'gallery_id',
+  foreignKey: 'user_id',
 });
 
 Comment.belongsTo(User, {
-  foreignKey: 'gallery_id',
+  foreignKey: 'user_id',
 });
 
 Post.hasMany(Comment, {
-  foreignKey: 'gallery_id',
+  foreignKey: 'post_id',
 });
 
 Comment.belongsTo(Post, {
-  foreignKey: 'gallery_id',
+  foreignKey: 'post_id',
 });
 
 module.exports = { User, Comment, Post };
