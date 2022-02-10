5721e801517c5758c75302089e3b763e76a5aab5
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-10 10:10:03 -0800

    create comment model

---
+++ models/Comment.js
@@ -0,0 +1,42 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comment extends Model {}
+
+Comment.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    comment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
+  },
+  {
+    // hooks: {
+    //   beforeCreate: async (newUserData) => {
+    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
+    //     return newUserData;
+    //   },
+    // },
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comment',
+  }
+);
+
+module.exports = Comment;
--- models/Post.js
+++ models/Post.js
@@ -1,11 +1,9 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
-class Post extends Model {
+class Post extends Model {}
 
-}
-
-User.init(
+Post.init(
   {
     id: {
       type: DataTypes.INTEGER,
@@ -13,18 +11,18 @@ User.init(
       primaryKey: true,
       autoIncrement: true,
     },
-    user_id: {
-      type: DataTypes.STRING,
-      allowNull: false,
-    },
     title: {
-      type: DataTypes.STRING,
-      allowNull: false,
+        type: DataTypes.STRING,
+        allowNull: false,
     },
     content: {
-      type: DataTypes.STRING(100,000),
-      allowNull: false,
+        type: DataTypes.STRING(100,000),
+        allowNull: false,
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    }
   },
   {
     // hooks: {
