0155d6174b9bb97366079e63596e0092676ce5a8
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-10 15:19:14 -0800

    create login.js logic, post route for api/users/login, and login.handlebars

---
+++ controllers/homeRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+const { User, Comment, Post } = require('../models');
+const withAuth = require('../utils/auth');
+
+// Homepage
+router.get('/', async (req, res) => {
+    // Send the rendered Handlebars.js template back as the response
+    res.render('homepage');
+  });
+
+// Login page
+router.get('/login', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+// Registration page
+router.get('/registration', (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+
+module.exports = router;
