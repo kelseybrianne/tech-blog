f1af3b8b3932897eb50cb7362b633959b2501235
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-12 09:59:28 -0800

    update definition of SequelizeStore with session.Store

--- server.js
+++ server.js
@@ -6,21 +6,20 @@ const exphbs = require('express-handlebars');
 
 
 const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// const sess = {
-//     secret: 'Super secret secret',
-//     cookie: {},
-//     resave: false,
-//     saveUninitialized: true,
-//     store: new SequelizeStore({
-//         db: sequelize
-//     })
-// }
-
+const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    store: new SequelizeStore({
+        db: sequelize
+    })
+}
 
 const hbs = exphbs.create({});
 
