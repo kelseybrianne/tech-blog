d2ec50190054f4acff8209064fe4b46e814547b8
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-14 15:04:07 -0800

    change blog post title size on homepage

--- seeds/postData.js
+++ seeds/postData.js
@@ -9,7 +9,7 @@ const postData = [
     },
     {
         id: 2,
-        title: 'BizzFuzz Algorithm',
+        title: 'BuzzFizz Algorithm',
         content: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Condimentum mattis pellentesque id nibh tortor. Pretium aenean pharetra magna ac. Urna nunc id cursus metus aliquam. Habitant morbi tristique senectus et netus et malesuada. Cursus mattis molestie a iaculis at. Dolor sit amet consectetur adipiscing. At elementum eu facilisis sed odio morbi quis. Justo donec enim diam vulputate ut pharetra sit. Sed vulputate mi sit amet. Scelerisque fermentum dui faucibus in ornare. Sagittis nisl rhoncus mattis rhoncus urna neque. Faucibus nisl tincidunt eget nullam non. A diam sollicitudin tempor id eu nisl. Eleifend mi in nulla posuere sollicitudin. Aenean et tortor at risus viverra adipiscing at in. Malesuada pellentesque elit eget gravida cum sociis. Nibh tortor id aliquet lectus proin nibh nisl condimentum.',
         user_id: '2'
     },
