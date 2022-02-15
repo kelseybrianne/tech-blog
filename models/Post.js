f84d970be0788c03472ee4bec76ba74a3ea0c140
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-14 21:31:47 -0800

    update Post.js

--- models/Post.js
+++ models/Post.js
@@ -30,12 +30,6 @@ Post.init(
     }
   },
   {
-    // hooks: {
-    //   beforeCreate: async (newUserData) => {
-    //     newUserData.password = await bcrypt.hash(newUserData.password, 10);
-    //     return newUserData;
-    //   },
-    // },
     sequelize,
     timestamps: false,
     freezeTableName: true,
