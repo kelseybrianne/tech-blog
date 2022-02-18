4387584d0317df20c203d2211783c8af2ebfb18e
Kelsey Mailau <kelseymailau@gmail.com>
2022-02-17 16:04:13 -0800

    add 'invalid username or password' alert to login.js

--- public/js/login.js
+++ public/js/login.js
@@ -11,10 +11,12 @@ const loginFormHandler = async (event) => {
       headers: { "Content-Type": "application/json" },
     });
 
+    console.log(response);
+
     if (response.ok) {
       document.location.replace("/");
     } else {
-      alert("Failed to log in");
+      $("#invalid").removeClass("hide");
     }
   }
 };
